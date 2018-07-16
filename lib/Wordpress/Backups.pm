b6ace12e18cb23d0efd2566e7c285c76a5870a38
brian d foy <brian.d.foy@gmail.com>
2018-07-16 13:59:17 -0400

    Make into a modulino and generalize

--- lib/Wordpress/Backups.pm
+++ lib/Wordpress/Backups.pm
@@ -49,38 +49,47 @@ You may redistribute this under the terms of the Artistic License 2.0.
 
 =cut
 
+unless( caller ) {
+	unless( @ARGV ) {
+		@ARGV = glob( '*/wp-config.php */public_html/wp-config.php' );
+		unshift @ARGV, 'wordpress_backups.d';
+		}
 
-@ARGV = glob( '*/wp-config.php */public_html/wp-config.php' );
-
-my $dir = "/home/mango/backups";
-mkdir $dir, 0700 unless -d $dir;
-die "Could not create backup dir!\n" unless -d $dir;
-
-foreach my $file ( glob( "$dir/*.gz" ) ) {
-	unlink $file if -M $file > 7;
+	run( @ARGV );
 	}
 
-foreach my $file ( @ARGV ) {
-	my $data = do { local( @ARGV, $/ ) = $file; <>; };
-	
-	my( $db )   = $data =~ m/define\(\s*'DB_NAME'\s*,\s*'(.*?)'\s*\)\s*;/;
-	my( $user ) = $data =~ m/define\(\s*'DB_USER'\s*,\s*'(.*?)'\s*\)\s*;/;
-	my( $pass ) = $data =~ m/define\(\s*'DB_PASSWORD'\s*,\s*'(.*?)'\s*\)\s*;/;
+sub run {
+	my( $dir, @files ) = @_;
 
-	my $time   = sprintf "%4d%02d%02d%02d%02d",
-		(localtime)[5] + 1900, (localtime)[4] + 1,
-		(localtime)[3,2,1,0];
+	mkdir $dir, 0700 unless -d $dir;
+	die "Could not create backup dir!\n" unless -d $dir;
 
-	my $result_file = catfile( $dir, "$db-$time.sql" );
+	foreach my $file ( glob( "$dir/*.gz" ) ) {
+		unlink $file if -M $file > 7;
+		}
 
-	system '/usr/local/bin/mysqldump',
-		'--add-drop-table',
-		'-u', $user,
-		qq(--password=$pass),
-		"--result-file=$result_file",
-		$db;
-		
-	system '/usr/bin/gzip', $result_file;
+	foreach my $file ( @files ) {
+		my $data = do { local( *ARGV, $/ ) = $file; <>; };
 
-	unlink $result_file;
+		my( $db )   = $data =~ m/define\(\s*'DB_NAME'\s*,\s*'(.*?)'\s*\)\s*;/;
+		my( $user ) = $data =~ m/define\(\s*'DB_USER'\s*,\s*'(.*?)'\s*\)\s*;/;
+		my( $pass ) = $data =~ m/define\(\s*'DB_PASSWORD'\s*,\s*'(.*?)'\s*\)\s*;/;
+
+		my $time   = sprintf "%4d%02d%02d%02d%02d",
+			(localtime)[5] + 1900, (localtime)[4] + 1,
+			(localtime)[3,2,1,0];
+
+		my $result_file = catfile( $dir, "$db-$time.sql" );
+
+		system '/usr/local/bin/mysqldump',
+			'--add-drop-table',
+			'-u', $user,
+			qq(--password=$pass),
+			"--result-file=$result_file",
+			$db;
+
+		system '/usr/bin/gzip', $result_file;
+
+		unlink $result_file;
+		}
 	}
